3aa6369ca899f92a4f6f41b792f01fe4f22536ce
MichaelNewell <navarre_2001@yahoo.com>
2018-02-07 09:50:42 -0500

    Commented reviewSlider.js

--- js/reviewSlider.js
+++ js/reviewSlider.js
@@ -9,6 +9,7 @@ function Slider( element ) {
 	this.init();
 }	
 
+/* -- Nav and Wrapper Function -- */
 Slider.prototype = {
 	init: function() {
 		this.links = this.el.querySelectorAll( "#slider-nav a" );
@@ -16,17 +17,20 @@ Slider.prototype = {
 		this.navigate();
 	},
 	navigate: function() {
-	
+		/* -- Compares current link to total link length and matches slide to it -- */	
 		for( var i = 0; i < this.links.length; ++i ) {
 			var link = this.links[i];
 			this.slide( link );	
 		}
 	},
 	
+	/* -- Slide behavior on nav click -- */
 	slide: function( element ) {
 		var self = this;
 		element.addEventListener( "click", function(e) {
+			/* -- Prevents scroll to screen top on nav click -- */
 			e.preventDefault();
+			/* -- Set clicked link to current on nav click -- */
 			var a = this;
 			self.setCurrentLink( a );
 			var index = parseInt( a.getAttribute( "review-slide" ), 10 ) + 1;
@@ -51,6 +55,7 @@ Slider.prototype = {
 	}	
 };
 
+/* -- Loads slider -- */
 document.addEventListener( "DOMContentLoaded", function() {
 	var aSlider = new Slider( "#slider" );
 	
