3004e59a6229e76fd80e45b3b1f1fced8282febd
MichaelNewell <navarre_2001@yahoo.com>
2018-02-09 12:51:05 -0500

    Quick review of slider code versus css

--- js/reviewSlider.js
+++ js/reviewSlider.js
@@ -2,14 +2,14 @@
   Javascript Slider for Client Testimonials
 ==================== */
 
-/* -- Slider Function -- */
+/* --- Slider Function --- */
 
 function Slider( element ) {
 	this.el = document.querySelector( element );
 	this.init();
 }	
 
-/* -- Nav and Wrapper Function -- */
+/* --- Nav and Wrapper Function --- */
 Slider.prototype = {
 	init: function() {
 		this.links = this.el.querySelectorAll( "#slider-nav a" );
@@ -17,20 +17,20 @@ Slider.prototype = {
 		this.navigate();
 	},
 	navigate: function() {
-		/* -- Compares current link to total link length and matches slide to it -- */	
+		/* --- Compares current link to total link length and matches slide to it --- */	
 		for( var i = 0; i < this.links.length; ++i ) {
 			var link = this.links[i];
 			this.slide( link );	
 		}
 	},
 	
-	/* -- Slide behavior on nav click -- */
+	/* --- Slide behavior on nav click --- */
 	slide: function( element ) {
 		var self = this;
 		element.addEventListener( "click", function(e) {
-			/* -- Prevents scroll to screen top on nav click -- */
+			/* --- Prevents scroll to screen top on nav click --- */
 			e.preventDefault();
-			/* -- Set clicked link to current on nav click -- */
+			/* --- Set clicked link to current on nav click --- */
 			var a = this;
 			self.setCurrentLink( a );
 			var index = parseInt( a.getAttribute( "review-slide" ), 10 ) + 1;
@@ -55,7 +55,7 @@ Slider.prototype = {
 	}	
 };
 
-/* -- Loads slider -- */
+/* --- Loads slider --- */
 document.addEventListener( "DOMContentLoaded", function() {
 	var aSlider = new Slider( "#slider" );
 	
